04c356218f5c8bb5042be73bc25ef8154618218b
m maruthi <maruthimarripati@gmail.com>
2020-07-12 16:12:55 +0530

    comments added

--- gitsample/src/gitsample/ForLoop.java
+++ gitsample/src/gitsample/ForLoop.java
@@ -1,9 +1,10 @@
 package gitsample;
-
+//sample program for for-loop
 public class ForLoop {
 public static void main(String args[])
 {
 	int j=0;
+	//for-loop
 	for(int i=0;i<20;i++)
 	{
 		System.out.print(i+",");
