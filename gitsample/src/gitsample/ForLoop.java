982193217e450960f577957cecb27181578c41d7
m maruthi <maruthimarripati@gmail.com>
2020-07-12 11:00:13 +0530

    whileloop added

--- gitsample/src/gitsample/ForLoop.java
+++ gitsample/src/gitsample/ForLoop.java
@@ -3,9 +3,15 @@ package gitsample;
 public class ForLoop {
 public static void main(String args[])
 {
+	int i1=0;
 	for(int i=0;i<20;i++)
 	{
 		System.out.print(i+",");
+	}
+	while(i1<=10)
+	{
+		System.out.println(i1);
+		i1++;
 	}
 }
 }
