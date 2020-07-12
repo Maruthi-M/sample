e74e396536f919874528c4d873fe18463035799e
m maruthi <maruthimarripati@gmail.com>
2020-07-12 08:53:40 +0530

    commit added

--- sample/src/sample/Example1.java
+++ sample/src/sample/Example1.java
@@ -4,6 +4,8 @@ public class Example1 {
 	public static void main(String args[])
 	{
 		System.out.println("helloo............welcome to git");
+		//commit
+		System.out.println("new line added");
 	}
 
 }
